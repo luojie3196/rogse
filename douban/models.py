71f08fe6f5427dbb109fc76054aa746e9fee5749
LuoJie <jieluo@tcl.com>
2017-03-17 15:16:43 +0800

    [douban]Init models

--- douban/models.py
+++ douban/models.py
@@ -1,3 +1,46 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
+from __future__ import unicode_literals
+
 from django.db import models
 
-# Create your models here.
+
+class DjangoMigrations(models.Model):
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class Douban(models.Model):
+    title = models.TextField()
+    movie_id = models.IntegerField()
+    rate = models.FloatField()
+    url = models.CharField(max_length=128)
+    cover = models.CharField(max_length=255)
+    director = models.TextField()
+    scriptwriter = models.TextField()
+    protagonist = models.TextField()
+    m_type = models.CharField(max_length=128)
+    region = models.TextField()
+    language = models.CharField(max_length=255)
+    release_time = models.TextField()
+    numbers = models.CharField(max_length=30)
+    run_time = models.CharField(max_length=128)
+    other_title = models.CharField(max_length=255)
+    imdb_link = models.CharField(max_length=128)
+    website = models.CharField(max_length=128)
+    comment_num = models.IntegerField()
+    summary = models.TextField()
+
+    class Meta:
+        managed = False
+        db_table = 'douban'
--- rogse/settings.py
+++ rogse/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'rogse.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_int',
+        'USER': 'int',
+        'PASSWORD': '93560',
+        'HOST': '172.16.34.142',
+        'PORT': '3306',
     }
 }
 
