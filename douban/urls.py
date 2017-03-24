39bf2e3074c44962003c56926c677b32d7420265
luojie <luojie3196@163.com>
2017-03-24 21:45:05 +0800

    douban web development stage 2

--- douban/urls.py
+++ douban/urls.py
@@ -13,11 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
-from django.contrib import admin
-from douban import urls as douban_urls
+from django.conf.urls import url
+from . import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^douban/', include(douban_urls)),
+    url(r'^home/', views.home),
+    url(r'^detail/(?P<page>\d+)$', views.detail),
+    url(r'^next_detail/$', views.next_detail),
 ]
