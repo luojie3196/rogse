7453e5acddf34fef7aea509b08020c62b55e9d74
LuoJie <jieluo@tcl.com>
2017-04-17 16:00:08 +0800

    douban web development stage 46

--- douban/context_processors.py
+++ douban/context_processors.py
@@ -10,4 +10,4 @@ def douban_obj(request):
         db_obj = Douban.objects.all()
     except Douban.DoesNotExist:
         raise Http404("Douban does not exist")
-    return {'douban_obj': db_obj,}
+    return {'douban_obj': db_obj, 'douban_obj_lens': db_obj.count()}
